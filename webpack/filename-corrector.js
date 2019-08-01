cb2e7e4a8fedeb18e922b1e378ca8ddb5f3939b0
Romanov Dmitry <RomanovDS@skblab.ru>
2019-08-01 11:07:14 +0500

    move fonts into other directory add common _styles.css add plugin filename-corrector

---
+++ webpack/filename-corrector.js
@@ -0,0 +1,5 @@
+module.exports = function(content) {
+    return content.replace(/url\s*\(\s*['"](?!data:)(.*?)['"]/g, (match, capture) => {
+        return match.split('%20').join(' ');
+    });
+};
