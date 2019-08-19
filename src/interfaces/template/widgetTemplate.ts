51d88453e0ccd71d85a20c3b0baad6969a575700
Romanov Dmitry <RomanovDS@skblab.ru>
2019-08-20 01:15:44 +0500

    added widgetTemplateConfig

--- src/interfaces/template/widgetTemplate.ts
+++ src/interfaces/template/widgetTemplate.ts
@@ -1,5 +1,6 @@
 import {DataSetTemplate} from './dataSetTemplate';
 import {ViewType} from "../../models/types";
+import {WidgetTemplateConfig} from "./widgetTemplateConfig";
 
 export interface WidgetTemplate {
     id: string | null;                      // ID шаблона (может быть null)
@@ -7,10 +8,7 @@ export interface WidgetTemplate {
     viewType: ViewType;                     // Тип виджета
     dataSets: Array<DataSetTemplate>;       // набор данных, каждый item описывает один набор данных, для одного графика/отчета
                                             // каждый item - это json DataSetTemplate
-    style?: {
-        background: string;
-        title: string;
-    };
+    style?: WidgetTemplateConfig;
     _links: {
         self: {
             href: string;
---
+++ src/interfaces/template/widgetTemplateConfig.ts
@@ -0,0 +1,4 @@
+export interface WidgetTemplateConfig {
+    background: string;
+    title: string;
+}
