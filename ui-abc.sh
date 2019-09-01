6ebb7884febd6b563ef81ecd1b1083519d858150
Romanov Dmitry <RomanovDS@skblab.ru>
2019-09-02 01:00:59 +0500

    remove old fonts

--- ui-abc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e;
-cd ~/VM/abc/widget-render/lib
-npm link # create a global symlink to the local "viking" project
-cd ~/VM/abc/template-adapter/front
-npm link abc-charts # create a symlink locally to global viking symlink
-# voila! now we can develop the two projects side-by-side without
-# having to worry about publishing either of them
