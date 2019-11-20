0da3447ab27d80b7f7cb9b062afbe06ffa85f7de
Romanov Dmitry <RomanovDS@skblab.ru>
2019-11-21 03:47:57 +0500

    copy webpack.prod

---
+++ webpack.prod-prev.js
@@ -0,0 +1,9 @@
+const merge = require('webpack-merge');
+
+module.exports = (env) => {
+    const common = require('./webpack.common.js')();
+
+    return merge(common, {
+        mode: 'production'
+    });
+}
