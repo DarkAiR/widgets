841bfe109e8f09f8c4ba27628325ab9b3ac5a992
Romanov Dmitry <RomanovDS@skblab.ru>
2020-04-01 17:21:53 +0500

    0.8.1 - Enable production mode for building to publishing - Update echarts 4.2.1 -> 4.7.0

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,6 @@ module.exports = (env) => {
     const common = require('./webpack.common.js')();
 
     return merge(common, {
-        mode: 'development'
+        mode: 'production'      // see config.optimization.minimize for more information
     });
 }
